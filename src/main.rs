ced38f226428946f5932a38e83027896ab6249e8
Paulo Henrique da Silva <paulo.silva1337@gmail.com>
2024-01-17 21:57:04 -0300

    0.0.1

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use escpos_rs::{EscposImage, Justification, Printer, PrinterProfile};
+use std::env;
+
+// 48 chars per line
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let text: &String = &args[1];
+
+    let printer_profile = PrinterProfile::usb_builder(0x1fc9, 0x2016).build();
+    let printer = match Printer::new(printer_profile) {
+        Ok(maybe_printer) => match maybe_printer {
+            Some(printer) => printer,
+            None => panic!("No printer was found :("),
+        },
+        Err(e) => panic!("Error: {}", e),
+    };
+
+    maybe_print_logo(&printer, &args);
+
+    match printer.println(text.replace("\\n", "\n")) {
+        Ok(_) => (),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn maybe_print_logo(printer: &Printer, args: &Vec<String>) {
+    if args.len() > 2 {
+        let logo: &String = &args[2];
+        let img = EscposImage::build_image(logo);
+        let escpos_image = EscposImage::new(img, 255, Justification::Center).unwrap();
+        match printer.image(escpos_image) {
+            Ok(_) => (), // Image should be printed
+            Err(e) => println!("Error: {}", e),
+        };
+    }
+}
