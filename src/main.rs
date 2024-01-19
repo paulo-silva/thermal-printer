df51fd8bf0ee6b934f3efc8eb36df055f9cfbf0d
Paulo Henrique da Silva <paulo.silva1337@gmail.com>
2024-01-18 22:25:35 -0300

    Add borders.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let text: &String = &args[1];
+    let border: String = "-".repeat(48);
 
     let printer_profile = PrinterProfile::usb_builder(0x1fc9, 0x2016).build();
     let printer = match Printer::new(printer_profile) {
@@ -18,8 +19,13 @@ fn main() {
         Err(e) => panic!("Error: {}", e),
     };
 
+    print_text(&printer, &border);
     maybe_print_logo(&printer, &args);
+    print_text(&printer, text);
+    print_text(&printer, &border);
+}
 
+fn print_text(printer: &Printer, text: &String) {
     match printer.println(text.replace("\\n", "\n")) {
         Ok(_) => (),
         Err(e) => println!("Error: {}", e),
@@ -30,7 +36,7 @@ fn maybe_print_logo(printer: &Printer, args: &Vec<String>) {
     if args.len() > 2 {
         let logo: &String = &args[2];
         let img = image::open(logo).unwrap();
-        let escpos_image = EscposImage::new(img, 128, Justification::Center).unwrap();
+        let escpos_image = EscposImage::new(img, 190, Justification::Left).unwrap();
         match printer.image(escpos_image) {
             Ok(_) => (), // Image should be printed
             Err(e) => println!("Error: {}", e),
